10eaea259d3f762c537f080fe956312fa7f39ecb
sampath11790 <bsampath11790@gmail.com>
2023-06-02 13:34:58 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./component/About/About";
 import Footer from "./component/Footer/Footer";
 import NavigationBar from "./component/Navigation/NavigationBar";
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar></NavigationBar>
+      <div>
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path="/About" element={<About />} />
+
+          {/* <Route path="contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+
       <Footer></Footer>
     </div>
   );
---
+++ src/component/About/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import cls from "./About.module.css";
+import { Box } from "@mui/material";
+const About = () => {
+  return (
+    <>
+      <div className={cls.aboutpage}>
+        <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-13modblock-oneBythree-A-menstripbanner-04May2023.jpg" />
+      </div>
+      <div className={cls.aboutpage}>
+        <img
+          src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Beauty_Desktop-2-30May2023.jpg"
+          id="hero_banner_image_1"
+          alt=""
+        />
+      </div>
+      <div className={cls.aboutpage}>
+        <img
+          class="MuiCardMedia-root jss11996 MuiCardMedia-media MuiCardMedia-img"
+          src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-shoes-9modularblock-A-26Apr2023.jpg"
+        />
+      </div>
+    </>
+  );
+};
+
+export default About;
